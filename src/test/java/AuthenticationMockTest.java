17c6934f265c5ce43105a5434e372daf338ea565
Freddy Aguilar Rojas <freddy.aguilar@et.bo>
2022-07-10 23:20:16 -0400

    Valid and invalid login mock tests

--- src/test/java/AuthenticationMockTest.java
+++ src/test/java/AuthenticationMockTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mockito;
 
 /**
@@ -21,9 +23,21 @@ public class AuthenticationMockTest {
         authentication = new Authentication();
 
         Mockito.when(credentialsServiceMock.isValidCredential("admin", "admin")).thenReturn(true);
-        Mockito.when(credentialsServiceMock.isValidCredential("admin", "pass")).thenReturn(false);
+        Mockito.when(credentialsServiceMock.isValidCredential("creator", "creator")).thenReturn(true);
+        Mockito.when(credentialsServiceMock.isValidCredential("reader", "reader")).thenReturn(true);
+        Mockito.when(credentialsServiceMock.isValidCredential("updater", "updater")).thenReturn(true);
+        Mockito.when(credentialsServiceMock.isValidCredential("deleter", "deleter")).thenReturn(true);
+        Mockito.when(credentialsServiceMock.isValidCredential("publisher", "publisher")).thenReturn(true);
+        Mockito.when(credentialsServiceMock.isValidCredential("moderator", "moderator")).thenReturn(true);
+        Mockito.when(credentialsServiceMock.isValidCredential("invalid", "invalid")).thenReturn(false);
 
         Mockito.when(permissionServiceMock.getPermission("admin")).thenReturn("C,R,U,D");
+        Mockito.when(permissionServiceMock.getPermission("creator")).thenReturn("C");
+        Mockito.when(permissionServiceMock.getPermission("reader")).thenReturn("R");
+        Mockito.when(permissionServiceMock.getPermission("updater")).thenReturn("U");
+        Mockito.when(permissionServiceMock.getPermission("deleter")).thenReturn("D");
+        Mockito.when(permissionServiceMock.getPermission("publisher")).thenReturn("C,R,U");
+        Mockito.when(permissionServiceMock.getPermission("moderator")).thenReturn("U,D");
 
         authentication.setCredentialsService(credentialsServiceMock);
         authentication.setPermissionService(permissionServiceMock);
@@ -32,17 +46,26 @@ public class AuthenticationMockTest {
     @AfterEach
     public void cleanUp() {}
 
-    @Test
-    public void verifyValidUser() {
-        String expectedResult = "user authenticated successfully with permission: [C,R,U,D]";
-        String obtainedResult = authentication.login("admin", "admin");
-        Assertions.assertEquals(expectedResult, obtainedResult, "ERROR LOGIN!");
+    @ParameterizedTest
+    @CsvSource({
+        "admin,admin,'C,R,U,D'",
+        "creator,creator,C",
+        "reader,reader,R",
+        "updater,updater,U",
+        "deleter,deleter,D",
+        "publisher,publisher,'C,R,U'",
+        "moderator,moderator,'U,D'",
+    })
+    public void verifyValidUser(String userName, String password, String permissions) {
+        String expectedResult = "user authenticated successfully with permission: [" + permissions + "]";
+        String obtainedResult = authentication.login(userName, password);
+        Assertions.assertEquals(expectedResult, obtainedResult, "ERROR valid user!");
     }
 
     @Test
     public void verifyInvalidUser() {
         String expectedResult = "user or password incorrect";
-        String obtainedResult = authentication.login("admin", "admin1");
-        Assertions.assertEquals(expectedResult, obtainedResult, "ERROR LOGIN!");
+        String obtainedResult = authentication.login("invalid", "invalid");
+        Assertions.assertEquals(expectedResult, obtainedResult, "ERROR Invalid user!");
     }
 }
